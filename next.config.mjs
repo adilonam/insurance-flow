715ef0ce9bbd4e7b3edb01848fe66222607bd504
Adil ABBADI <adil.abbadi.1996@gmail.com>
2025-11-09 17:42:44 +0000

    chore: add Prisma plugin to Next.js configuration and update build script for webpack compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+  turbopack: {},
 }
 
 export default nextConfig
